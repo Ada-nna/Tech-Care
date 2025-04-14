b756f0a6fc184a6040fd663f0d6d53647114d15d
Ada-nna <cyndigal2@gmail.com>
2025-04-14 20:13:35 +0100

    screen size adjusted for mobile blocker

--- src/components/MobileBlocker .tsx
+++ src/components/MobileBlocker .tsx
@@ -1,6 +1,6 @@
 const MobileBlocker = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen overflow-hidden px-6 bg-gray-100 text-center">
+    <div className="sm:hidden w-full h-screen flex flex-col items-center justify-center px-4 bg-gray-100 text-center">
       <div className="text-[#072635]">
         <div className="flex items-center justify-center my-10">
           <img src="/TestLogo.svg" alt="TestLogo" className="h-12 w-auto" />
