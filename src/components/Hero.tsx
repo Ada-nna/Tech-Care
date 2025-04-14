e926fb442bf418f160490a9d9f0b54f2835989e3
Ada-nna <cyndigal2@gmail.com>
2025-04-14 20:05:26 +0100

    clgs removed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,9 +15,6 @@ const Hero = () => {
   const auth = import.meta.env.VITE_API_AUTH;
 
   useEffect(() => {
-    console.log("URL:", url);
-    console.log("AUTH:", auth);
-
     const fetchPatients = async () => {
       try {
         const response = await axios.get(url, {
